217a6608d2457ad858342b84dc324394605af4fc
Vyasss <97939517+Vyasss@users.noreply.github.com>
2025-11-29 00:02:13 +0530

    Update check_models.py

--- check_models.py
+++ check_models.py
@@ -5,7 +5,7 @@ import google.generativeai as genai
 load_dotenv()
 genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
 
-print("Checking available models...")6
+print("Checking available models...")2
 try:
     for m in genai.list_models():
         if 'generateContent' in m.supported_generation_methods:
