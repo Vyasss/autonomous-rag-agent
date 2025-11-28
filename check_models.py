0239f0d81b2bb6851ea8e30eeb0eebb1d1d282a3
Vyasss <97939517+Vyasss@users.noreply.github.com>
2025-11-29 00:42:21 +0530

    Update check_models.py

--- check_models.py
+++ check_models.py
@@ -4,7 +4,6 @@ import google.generativeai as genai
 
 load_dotenv()
 genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
-
 print("Checking available models...")
 try:
     for m in genai.list_models():
